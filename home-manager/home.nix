da25fcb5f4361b914e029e14bd9c0d7585003437
Pravin <pravin@pravinxor.dev>
2025-12-11 22:28:50 -0600

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,35 +3,49 @@
 	imports = [ ./editor.nix ./shell.nix ];
 	home.packages = with pkgs; [
 		git
-		yt-dlp
 		ffmpeg
-		aria2
-		mpv
 	];
 	
 	programs = {
-		ssh = {
-			enable = true; enableDefaultConfig = false;
-			matchBlocks."*" = {
-				addKeysToAgent = "yes";
-				extraOptions.UseKeychain = "yes";
+		mpv = {
+			enable = true;
+			config = {
+				hwdec = "auto";
 			};
 		};
-
-		direnv =  { enable = true; nix-direnv.enable = true; enableZshIntegration = true; };
+		yt-dlp = {
+			enable = true;
+		};
+		gemini-cli = {
+			enable = true;
+			settings = {
+				vimMode = true;
+				preferredEditor = "nvim";
+				theme = "Xcode";
+			};
+		};
+		git = {
+			enable = true;
+			settings = {
+				user = { name = "Pravin Ramana"; email = "pravin@pravinxor.dev"; };
+			};
+		};
+		ssh = {
+			enable = true; enableDefaultConfig = false;
+			matchBlocks."*" = { addKeysToAgent = "yes"; extraOptions.UseKeychain = "yes"; };
+		};
 	};
-	xdg.configFile."nixpkgs/config.nix".text = ''
-		{
-			allowUnfree = true;
-		}
-	'';	
+	xdg.configFile."nixpkgs/config.nix".text = "{ allowUnfree = true; }";
 		
 	home = {
 		shell.enableZshIntegration = true;
 		username = "pravin"; homeDirectory = "/Users/pravin";
 		stateVersion = "25.05";
 	};
+	nix.gc = { automatic = true; persistent = true; };
 
-  	nixpkgs.config.allowUnfree = true;
+  	nixpkgs.config = {
+		allowUnfree = true;
+	};
   	programs.home-manager.enable = true;
 }
