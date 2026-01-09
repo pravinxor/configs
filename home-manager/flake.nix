89f8a7504f0515cd9ac200d950439967811f85be
Pravin <pravin@pravinxor.dev>
2026-01-09 10:57:39 -0600

    Update flake.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,29 +1,29 @@
 {
-  description = "Home Manager configuration of pravin";
+	description = "Home Manager configuration of pravin";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+	inputs = {
+		nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?ref=nixos-unstable&shallow=1";
+    	home-manager = { url = "git+https://github.com/nix-community/home-manager?shallow=1"; inputs.nixpkgs.follows = "nixpkgs"; };
+    	nixvim = { url = "git+https://github.com/nix-community/nixvim?shallow=1"; inputs.nixpkgs.follows = "nixpkgs"; };
+	};
 
-  outputs = { nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  	outputs = { nixpkgs, home-manager, ... }@inputs:
+		let
+			system = "aarch64-darwin";
+			pkgs = import nixpkgs {
+				inherit system;
+				config = {
+				  allowUnfree = true;
+				  allowUnsupportedSystem = true;
+				};
+			};
+		in
     {
-      homeConfigurations."pravin" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        # Pass 'inputs' to all sub-modules (home.nix, editor.nix, etc.)
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home.nix ];
-      };
-    };
+    	homeConfigurations."pravin" = home-manager.lib.homeManagerConfiguration {
+			inherit pkgs;
+			# Pass 'inputs' to all sub-modules (home.nix, editor.nix, etc.)
+			extraSpecialArgs = { inherit inputs; };
+			modules = [ ./home.nix ];
+    	};
+	};
 }
