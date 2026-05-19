2c91885b5cc812bfec7499a71d828cbf11c888c6
Pravin <pravin@pravinxor.dev>
2026-05-19 12:24:10 -0500

    Update editor.nix

--- home-manager/editor.nix
+++ home-manager/editor.nix
@@ -7,75 +7,75 @@
 		viAlias = true; vimDiffAlias = true; vimAlias = true;
 		nixpkgs.useGlobalPackages = true;
 		enableMan = false;
-
+		
+		colorschemes.melange.enable = true;
 		clipboard.providers.pbcopy.enable = true;
-		colorschemes.melange = {
-			enable = true;
-		};
 
 		dependencies.ripgrep.enable = true;
 
-		performance.byteCompileLua = {
-			enable = true;
-			initLua = true;
-			nvimRuntime = true;
-			plugins = true;
+		performance = {
+			byteCompileLua = {
+				enable = true;
+				configs = true;
+				initLua = true;
+				luaLib = true;
+				nvimRuntime = true;
+				plugins = true;
+			};
+			combinePlugins.enable = true;
 		};
 
 		opts = {
 			clipboard = "unnamedplus";
 			background = "light";
-			number = true;
-			relativenumber = true;
 			shiftwidth = 4;
 			tabstop = 4;
 			expandtab = false;
 
-			foldmethod = "expr"; foldexpr = "v:lua.vim.treesitter.foldexpr()"; foldlevel = 99;
+			foldmethod = "expr"; foldexpr = "v:lua.vim.lsp.foldexpr()"; foldlevel = 99;
+
+			number = true; relativenumber = true;
 
 			smartindent = true; autoindent = true;
 			ignorecase = true; smartcase = true;
 		};
 
-		lsp.servers = {
-			svelte.enable = true; tailwindcss.enable = true;
-			wgsl_analyzer.enable = true;
-			pyright.enable = true;
-			vtsls.enable = true;
-			clangd.enable = true;
-			zls.enable = true;
-			nixd.enable = true;
+		lsp = {
+			servers = {
+				svelte.enable = true; tailwindcss.enable = true; vtsls.enable = true;
+				ocamllsp.enable = true;
+				ty.enable = true; ruff.enable = true;
+				clangd.enable = true;
+				nixd.enable = true;
+			};
 		};
 
 		plugins = {
 			lspconfig.enable = true;
 			direnv.enable = true;
-			blink-cmp.enable = true;
-			treesitter = {
-				enable = true;
-				grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-					bash javascript python typescript
-					c cpp wgsl zig
-					html json svelte toml yaml nix
-				];
-				settings = {
-					indent.enable = false;
-					folding.enable = true;
-					highlight = {
-						additional_vim_regex_highlighting = false;
-						enable = true;
-					};
-				};
-			};
-			treesitter-context.enable = true;
 			web-devicons.enable = false;
 			telescope = {
 				enable = true;
 				keymaps = {
-					"<leader>f" = "find_files";
-					"<leader>g" = "live_grep";
-					"<leader>b" = "buffers";
-					"<leader>h" = "help_tags";
+					"<leader>ff".action = "find_files";
+					"<leader>fg".action = "live_grep";
+					"<leader>fb".action = "buffers";
+					"<leader>fd".action = "diagnostics";
+				};
+			};
+			treesitter = {
+				enable = true;
+				grammarPackages = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+					bash javascript python typescript ocaml
+					c cpp
+					html json svelte toml yaml nix
+				];
+				settings = {
+					indent.enable = true;
+					highlight = {
+						additional_vim_regex_highlighting = false;
+						enable = true;
+					};
 				};
 			};
 		};
