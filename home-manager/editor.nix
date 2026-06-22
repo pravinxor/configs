4eeff7b8db22c99a3ee9b5cef541d5078586b187
Pravin Ramana <pravin@pravinxor.dev>
2026-06-22 09:28:18 -0500

    update editor

--- home-manager/editor.nix
+++ home-manager/editor.nix
@@ -1,58 +1,74 @@
 { pkgs, inputs, ... }:
 {
-	imports = [ inputs.nixvim.homeModules.nixvim ];
+    imports = [ inputs.nixvim.homeModules.nixvim ];
 
-	programs.nixvim = {
-		enable = true; defaultEditor = true;
-		viAlias = true; vimAlias = true;
-		nixpkgs.useGlobalPackages = true;
-		enableMan = false;
-		
-		colorschemes.melange.enable = true;
-		clipboard.providers.pbcopy.enable = true;
+    programs.nixvim = {
+        enable = true; defaultEditor = true;
+        viAlias = true; vimAlias = true;
+        nixpkgs.useGlobalPackages = true;
+        enableMan = false;
+        
+        colorschemes.melange.enable = true;
+        clipboard.providers.pbcopy.enable = true;
+        performance.byteCompileLua = {
+            enable = true;
+            luaLib = true;
+            nvimRuntime = true;
+            plugins = true;
+        };
 
-		opts = {
-			clipboard = "unnamedplus";
-			background = "light";
-			shiftwidth = 4;
-			tabstop = 4;
+        autoCmd = [
+            {
+                command = "GuessIndent";
+                event = [ "BufEnter" ];
+            }
+        ];
 
-			foldmethod = "expr"; foldexpr = "v:lua.vim.lsp.foldexpr()"; foldlevel = 99;
+        opts = {
+            clipboard = "unnamedplus";
+            background = "light";
+            foldmethod = "expr"; foldexpr = "v:lua.vim.lsp.foldexpr()"; foldlevel = 99;
 
-			number = true; relativenumber = true;
+            number = true; relativenumber = true;
 
-			smartindent = true; autoindent = true;
-			ignorecase = true; smartcase = true;
-		};
+            cindent = true;
+            ignorecase = true; smartcase = true;
+        };
 
-		lsp = {
-			servers = {
-				svelte.enable = true; tailwindcss.enable = true; vtsls.enable = true;
-				ocamllsp.enable = true;
-				ty.enable = true; ruff.enable = true;
-				clangd.enable = true;
-				nixd.enable = true;
-			};
-		};
+        lsp = {
+            servers = {
+                svelte.enable = true; tailwindcss.enable = true; vtsls.enable = true;
+                ocamllsp.enable = true;
+                ty.enable = true; ruff.enable = true;
+                clangd.enable = true;
+                nixd.enable = true;
+            };
+        };
 
-		plugins = {
-			lspconfig.enable = true;
-			direnv.enable = true;
-			treesitter = {
-				enable = true;
-				grammarPackages = with pkgs.vimPlugins.nvim-treesitter-parsers; [
-					bash javascript python typescript ocaml
-					c cpp
-					html json svelte toml yaml nix
-				];
-				settings = {
-					indent.enable = true;
-					highlight = {
-						additional_vim_regex_highlighting = false;
-						enable = true;
-					};
-				};
-			};
-		};
-	};
+        plugins = {
+            lspconfig.enable = true;
+            blink-cmp = {
+                enable = true;
+                settings = {
+                    signature.enabled = true;
+                    sources.default = [ "lsp" "path" ];
+                };
+            };
+            direnv.enable = true;
+            guess-indent = {
+                enable = true;
+                callSetup = true;
+            };
+            treesitter = {
+                enable = true;
+                grammarPackages = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+                    bash javascript python typescript ocaml make
+                    c cpp
+                    html json svelte toml yaml nix
+                ];
+                highlight.enable = true;
+                indent.enable = true;
+            };
+        };
+    };
 }
